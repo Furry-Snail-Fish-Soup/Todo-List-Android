016e63905a7aad3fbe8e0640d1c585301143681f
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2022-04-16 17:09:20 +0800

    完成教程部分。

--- app/src/main/java/fun/sast/todolist/MainActivity.kt
+++ app/src/main/java/fun/sast/todolist/MainActivity.kt
@@ -1,60 +1,70 @@
 package `fun`.sast.todolist
 
 import `fun`.sast.todolist.ui.theme.TodoListTheme
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import kotlin.collections.List
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val todoItems = mutableListOf(
-            TodoItemViewModel(false, "Not finished"),
-            TodoItemViewModel(true, "finished")
-        )
         setContent {
             TodoListTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    MyApp()
-                }
+                Surface(modifier = Modifier.fillMaxSize()) { MyApp() }
             }
         }
     }
 }
 
 @Composable
-fun MyApp(names: List<String> = listOf("World", "Compose")) {
-    Column {
-        for (name in names) {
-            Greeting(name = name)
+fun MyApp() {
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen { shouldShowOnboarding = false }
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun Greetings(
+    names: List<String> = List(1000) { "$it" }
+) {
+    LazyColumn {
+        items(items = names) {
+            Greeting(name = it)
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    var expanded by remember { mutableStateOf(false) }
-    val extraPadding = if (expanded) 48.dp else 0.dp
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(if (expanded) 480.dp else 0.dp)
     Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
+        color = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
@@ -63,11 +73,9 @@ fun Greeting(name: String) {
                     .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.displayLarge)
             }
-            OutlinedButton(onClick = {
-                expanded = !expanded
-            }) {
+            OutlinedButton(onClick = { expanded = !expanded }) {
                 Text(
                     text = if (expanded) "Show less" else "Show more"
                 )
@@ -77,7 +85,36 @@ fun Greeting(name: String) {
 }
 
 @Preview(showBackground = true, widthDp = 320)
+@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES, widthDp = 320, name = "DefaultDark")
 @Composable
 fun DefaultPreview() {
     MyApp()
 }
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+//
+//@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+//@Composable
+//fun OnboardingPreview() {
+//    TodoListTheme {
+//        OnboardingScreen(onContinueClicked = {})
+//    }
+//}
