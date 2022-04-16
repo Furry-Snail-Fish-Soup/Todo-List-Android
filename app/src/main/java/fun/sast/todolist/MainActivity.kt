8605c3bd65dfa7918b8c03fdeff3e1ef1546611e
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2022-04-16 22:17:53 +0800

    新的开始……

--- app/src/main/java/fun/sast/todolist/MainActivity.kt
+++ app/src/main/java/fun/sast/todolist/MainActivity.kt
@@ -1,123 +1,42 @@
 package `fun`.sast.todolist
 
 import `fun`.sast.todolist.ui.theme.TodoListTheme
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TodoListTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.surface,
-                    contentColor = contentColorFor(MaterialTheme.colorScheme.surface)
-                ) { MyApp() }
+
+                ModalNavigationDrawer(drawerContent = {
+                    Surface {
+                        Text(text = "drawerContent")
+                    }
+                }) {
+                    Scaffold(topBar = {
+                        Surface {
+                            Text(text = "topBar")
+                        }
+                    }, bottomBar = {
+                        Surface {
+                            Text(text = "bottomBar")
+                        }
+                    }, snackbarHost = {
+                        Surface {
+                            Text(text = "snackbarHost")
+                        }
+                    }) {
+                        Surface {
+                            Text(text = "content")
+                        }
+                    }
+                }
             }
         }
     }
 }
-
-@Composable
-fun MyApp() {
-    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
-
-    if (shouldShowOnboarding) {
-        OnboardingScreen { shouldShowOnboarding = false }
-    } else {
-        Greetings()
-    }
-}
-
-@Composable
-fun Greetings(
-    names: List<String> = List(1000) { "$it" }
-) {
-    LazyColumn {
-        items(items = names) {
-            Greeting(name = it)
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    var expanded by rememberSaveable { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(if (expanded) 480.dp else 0.dp)
-    Surface(
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp),
-        color = MaterialTheme.colorScheme.primary,
-        contentColor = MaterialTheme.colorScheme.onPrimary
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(bottom = extraPadding)
-            ) {
-                Text(text = "Hello,")
-                Text(text = name, style = MaterialTheme.typography.displayLarge)
-            }
-            OutlinedButton(onClick = { expanded = !expanded }) {
-                Text(
-                    text = if (expanded) "Show less" else "Show more"
-                )
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true, widthDp = 320)
-@Preview(showBackground = true, uiMode = UI_MODE_NIGHT_YES, widthDp = 320, name = "DefaultDark")
-@Composable
-fun DefaultPreview() {
-    MyApp()
-}
-
-@Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit) {
-
-    Surface(
-        color = MaterialTheme.colorScheme.surface,
-        contentColor = contentColorFor(MaterialTheme.colorScheme.surface)
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text("Welcome to the Basics Codelab!")
-            Button(
-                modifier = Modifier.padding(vertical = 24.dp),
-                onClick = onContinueClicked
-            ) {
-                Text("Continue")
-            }
-        }
-    }
-}
-//
-//@Preview(showBackground = true, widthDp = 320, heightDp = 320)
-//@Composable
-//fun OnboardingPreview() {
-//    TodoListTheme {
-//        OnboardingScreen(onContinueClicked = {})
-//    }
-//}
