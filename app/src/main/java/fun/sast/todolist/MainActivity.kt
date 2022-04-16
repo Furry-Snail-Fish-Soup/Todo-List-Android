4556ab0ab50cabc17dd850d74ebb34fa642fba30
极速蜗牛 <31986081+Jisu-Woniu@users.noreply.github.com>
2022-04-17 03:15:19 +0800

    图标修改、界面整理

--- app/src/main/java/fun/sast/todolist/MainActivity.kt
+++ app/src/main/java/fun/sast/todolist/MainActivity.kt
@@ -6,15 +6,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,67 +25,105 @@ class MainActivity : ComponentActivity() {
             MainUI()
         }
     }
+}
 
-    @OptIn(ExperimentalMaterial3Api::class)
-    @Composable
-    private fun MainUI() {
-        TodoListTheme {
-            ModalNavigationDrawer(drawerContent = {
-                Surface(color = Color.Red, modifier = Modifier.fillMaxWidth()) {
-                    Text(text = "drawerContent")
-                }
-            }, drawerShape = Shapes.large) {
-                Scaffold(topBar = {
-                    CenterAlignedTopAppBar(
-                        navigationIcon = { Icon(Icons.TwoTone.Menu, "菜单") },
-                        title = { Text("topBar") },
-                        actions = { Icon(Icons.TwoTone.Search, "搜索") },
-                        modifier = Modifier
-                    )
-                }, bottomBar = {
-                    BottomBar()
-                }, floatingActionButton = {
-                    FloatingActionButton(
-                        shape = androidx.compose.material3.Shapes.Full,
-                        onClick = { /*TODO*/ }
-                    ) {
-                        Icon(Icons.TwoTone.Add, "新建待办")
-                    }
-                }) {
-                    Surface(modifier = Modifier.fillMaxSize()) {
-                        LazyColumn {
-                            items(listOf(0, 1, 2)) {
-                                Text("$it")
-                            }
-                        }
-                    }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun MainUI() {
+    TodoListTheme {
+        ModalNavigationDrawer(
+            drawerContent = {
+                Text(text = "标题", modifier = Modifier.align(Alignment.Start))
+                NavigationDrawerItem(
+                    label = { Text(text = "全部") },
+                    selected = false,
+                    onClick = { /*TODO*/ },
+                    icon = { Icon(Icons.TwoTone.AllInclusive, "All") })
+                NavigationDrawerItem(
+                    label = { Text(text = "今日提醒") },
+                    selected = false,
+                    onClick = { /*TODO*/ },
+                    icon = { Icon(Icons.TwoTone.CalendarToday, "All") })
+                NavigationDrawerItem(
+                    label = { Text(text = "全部提醒") },
+                    selected = false,
+                    onClick = { /*TODO*/ },
+                    icon = { Icon(Icons.TwoTone.Alarm, "All") })
+            },
+            drawerShape = Shapes.large
+        ) { MainPart() }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview
+private fun MainPart() {
+    Scaffold(
+        topBar = { TopBar() },
+        bottomBar = { BottomBar() },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                shape = RoundedCornerShape(16.dp)
+            ) { Icon(Icons.TwoTone.Add, "新建待办") }
+        }) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            LazyColumn {
+                items(listOf(0, 1, 2)) {
+                    Text("$it")
                 }
             }
         }
     }
+}
 
-    @Composable
-    private fun BottomBar() {
-        NavigationBar {
-            NavigationBarItem(
-                selected = true,
-                icon = { Icon(Icons.TwoTone.List, "List") },
-                label = { Text(text = "总览") },
-                onClick = { /*TODO*/ })
-            NavigationBarItem(
-                selected = false,
-                icon = { Icon(Icons.TwoTone.Tag, "List") },
-                label = {
-                    Text(
-                        text = "标签"
-                    )
-                },
-                onClick = { /*TODO*/ })
-            NavigationBarItem(
-                selected = false,
-                icon = { Icon(Icons.TwoTone.History, "List") },
-                label = { Text(text = "历史") },
-                onClick = { /*TODO*/ })
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview(widthDp = 297)
+private fun Drawer() {
+
+}
+
+@Composable
+@Preview
+private fun TopBar() {
+    CenterAlignedTopAppBar(
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.TwoTone.Menu, "菜单")
+            }
+        },
+        title = { Text("topBar") },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.TwoTone.Search, "搜索")
+            }
         }
+    )
+}
+
+@Composable
+private fun BottomBar() {
+    NavigationBar {
+        NavigationBarItem(
+            selected = true,
+            icon = { Icon(Icons.TwoTone.List, "List") },
+            label = { Text(text = "总览") },
+            onClick = { /*TODO*/ })
+        NavigationBarItem(
+            selected = false,
+            icon = { Icon(Icons.TwoTone.Tag, "List") },
+            label = {
+                Text(
+                    text = "标签"
+                )
+            },
+            onClick = { /*TODO*/ })
+        NavigationBarItem(
+            selected = false,
+            icon = { Icon(Icons.TwoTone.History, "List") },
+            label = { Text(text = "历史") },
+            onClick = { /*TODO*/ })
     }
 }
